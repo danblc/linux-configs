00f0aa1e9c0221166c848e6b81c06ca6366787f2
danblc <dan.belceanu@gmail.com>
2024-06-16 21:15:20 +0300

    init

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+require("danblc.plugins-setup")
+require("danblc.core.options")
+require("danblc.core.keymaps")
+require("danblc.core.colorscheme")
+require("danblc.plugins.comment")
+require("danblc.plugins.nvim-tree")
+require("danblc.plugins.lualine")
+require("danblc.plugins.undotree")
